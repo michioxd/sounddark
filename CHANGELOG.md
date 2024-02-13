bbfac0031ef76ff0fe6208d0e504923d104cee56
michioxd <michio.haiyaku@gmail.com>
2024-02-13 19:22:56 +0700

    0.1.8.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.8.2] - 2024-02-13
+
+- Change logo due to Microsoft Store 1.1.2 policies
+
 ## [0.1.8.1] - 2024-01-28
 
 - Fix Menu is not showing in Firefox browser
--- src/sounddark.js
+++ src/sounddark.js
@@ -2,7 +2,7 @@
  * 
  * @name SoundDark
  * @description A Chromium extension to get better Dark theme for SoundCloud
- * @version 0.1.8.1
+ * @version 0.1.8.2
  * @license https://github.com/michioxd/sounddark/blob/main/LICENSE
  * @author michioxd
  * 
@@ -19,7 +19,7 @@ function getBrowserPlatform() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const version = "0.1.8.1";
+    const version = "0.1.8.2";
     const changeThemeQuery = self.matchMedia("(prefers-color-scheme: dark)");
 
     console.log(`SoundDark v${version} - https://github.com/michioxd/sounddark\nLocal saved: ${localStorage.getItem("SoundDark_mode") ?? "no"}\nSystem color: ${changeThemeQuery.matches ? "dark" : "light"}\nPlatfrom: ${getBrowserPlatform() === 2 ? "firefox" : getBrowserPlatform() === 1 ? "chromium" : "unknown"}`);
