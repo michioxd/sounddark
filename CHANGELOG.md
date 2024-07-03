347cc7546d9af225eb12548e936b3a3f0b6fcdd8
michioxd <michio.haiyaku@gmail.com>
2024-07-03 17:07:59 +0700

    0.1.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.9] - 2024-07-03
+
+- Fix backgroud color in big player. [Example](https://soundcloud.com/ruqoa/sweet-crepe).
+- Fix sound badge artwork in playlist.
+
 ## [0.1.8.3] - 2024-06-09
 
 - Fix border color for each notification item.
--- src/sounddark.js
+++ src/sounddark.js
@@ -2,7 +2,7 @@
  * 
  * @name SoundDark
  * @description A Chromium extension to get better Dark theme for SoundCloud
- * @version 0.1.8.3
+ * @version 0.1.9
  * @license https://github.com/michioxd/sounddark/blob/main/LICENSE
  * @author michioxd
  * 
@@ -19,7 +19,7 @@ function getBrowserPlatform() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const version = "0.1.8.3";
+    const version = "0.1.9";
     const changeThemeQuery = self.matchMedia("(prefers-color-scheme: dark)");
 
     console.log(`SoundDark v${version} - https://github.com/michioxd/sounddark\nLocal saved: ${localStorage.getItem("SoundDark_mode") ?? "no"}\nSystem color: ${changeThemeQuery.matches ? "dark" : "light"}\nPlatfrom: ${getBrowserPlatform() === 2 ? "firefox" : getBrowserPlatform() === 1 ? "chromium" : "unknown"}`);
